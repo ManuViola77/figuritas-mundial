3ed4026302ea985ea5fc4b12f3030fe5f163f71c
Manuela Viola <manuela.viola@rootstrap.com>
2022-09-24 19:13:02 -0300

    feat: add data structure

---
+++ src/data/marruecos.js
@@ -0,0 +1,88 @@
+const marruecos = [
+  {
+    id: 0,
+    image: "",
+    have: false,
+    haveGold: false,
+    name: "",
+  },
+  {
+    id: 1,
+    image: "",
+    have: false,
+    haveGold: false,
+    name: "",
+  },
+  {
+    id: 2,
+    image: "",
+    have: false,
+    haveGold: false,
+    name: "",
+  },
+  {
+    id: 3,
+    image: "",
+    have: false,
+    haveGold: false,
+    name: "",
+  },
+  {
+    id: 4,
+    image: "",
+    have: false,
+    haveGold: false,
+    name: "",
+  },
+  {
+    id: 5,
+    image: "",
+    have: false,
+    haveGold: false,
+    name: "",
+  },
+  {
+    id: 6,
+    image: "",
+    have: false,
+    haveGold: false,
+    name: "",
+  },
+  {
+    id: 7,
+    image: "",
+    have: false,
+    haveGold: false,
+    name: "",
+  },
+  {
+    id: 8,
+    image: "",
+    have: false,
+    haveGold: false,
+    name: "",
+  },
+  {
+    id: 9,
+    image: "",
+    have: false,
+    haveGold: false,
+    name: "",
+  },
+  {
+    id: 10,
+    image: "",
+    have: false,
+    haveGold: false,
+    name: "",
+  },
+  {
+    id: 11,
+    image: "",
+    have: false,
+    haveGold: false,
+    name: "",
+  },
+];
+
+export default marruecos;
